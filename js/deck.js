1c5ed792679eaf3617ab5cb46129b571286b1df7
Matthew Farncomb <mfarncomb101@gmail.com>
2022-02-17 16:27:10 +0200

    Refactor Deck a little

--- js/deck.js
+++ js/deck.js
@@ -3,11 +3,13 @@ class Deck {
     #source_language;
     #target_language;
     #cards;
+    #drawPile;
     
     constructor(source_language, target_language) {
         this.#source_language = source_language;
         this.#target_language = target_language;
         this.#cards = [];
+        this.#drawPile = this.#cards;
         // this.drawPi = this.drawPile();
     }
 
@@ -15,52 +17,86 @@ class Deck {
         this.#cards.push(card);
     }
 
+   
+
+    // drawCard(index) {
+    //     const card = drawPile[index];
+    //     card.inHand = true;
+    //     return card;
+    // }
+
+    // testing
+
     drawCard(index) {
-        const card = drawPile[index];
-        card.inHand = true;
-        return card;
+        try {
+            const nextCard = this.#drawPile[index];
+            this.#drawPile = this.#drawPile.filter(card => card.word.word != nextCard.word.word);
+            return nextCard;
+        } catch (error) {
+            console.error(`DrawPile is only ${this.#drawPile.length} which is shorter than the index of ${index}`);
+        }
     }
 
     randomCard() {
-        
-        const drawPile = this.drawPile();
-        const length = drawPile.length;
+        const length = this.#drawPile.length;
         if (length < 1) {
             this.shuffle();
             return this.randomCard();
         }
         else {
+            // console.log(this.#drawPile);
             const index = getRandomInt(0, length);
-            console.log(drawPile);
-            const nextCard = drawPile[index];
-            console.log(nextCard)
-            nextCard.inHand = true;
-            return nextCard;
+            return this.drawCard(index);
+            // const nextCard = this.#drawPile[index];
+            // this.#drawPile = this.#drawPile.filter(card => card.word.word != nextCard.word.word);
+            // return nextCard;
         }
     }
 
     shuffle() {
-        this.#cards.forEach((card) => {
-            card.inHand = false;
-            card.discarded = false;
-        }); 
+        this.#drawPile = this.#cards;
     }
 
-    drawPile() {
-        return this.#cards.filter((card) => {
-            if (!card.discarded && !card.inHand) {
-              return card;
-            }
-        });
-    }
+    // randomCard() {
+        
+    //     const drawPile = this.drawPile();
+    //     const length = drawPile.length;
+    //     if (length < 1) {
+    //         this.shuffle();
+    //         return this.randomCard();
+    //     }
+    //     else {
+    //         const index = getRandomInt(0, length);
+    //         console.log(drawPile);
+    //         const nextCard = drawPile[index];
+    //         console.log(nextCard)
+    //         nextCard.inHand = true;
+    //         return nextCard;
+    //     }
+    // }
 
-    discardPile() {
-        return this.#cards.filter((card) => {
-            if (card.discarded) {
-              return card;
-            }
-        });
-    }
+    // shuffle() {
+    //     this.#cards.forEach((card) => {
+    //         card.inHand = false;
+    //         card.discarded = false;
+    //     }); 
+    // }
+
+    // drawPile() {
+    //     return this.#cards.filter((card) => {
+    //         if (!card.discarded && !card.inHand) {
+    //           return card;
+    //         }
+    //     });
+    // }
+
+    // discardPile() {
+    //     return this.#cards.filter((card) => {
+    //         if (card.discarded) {
+    //           return card;
+    //         }
+    //     });
+    // }
 
     getDeck(server, count) {
         server.fetchDeck(count, this.#source_language, this.#target_language).then((data) => {
